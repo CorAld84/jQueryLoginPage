4730ead82675061939d24e74716ef9ba1efe4039
CorAld84 <aldocoroneo@gmail.com>
2022-09-26 17:20:18 +0100

    jquery modified push

---
+++ js/login.js
@@ -0,0 +1,106 @@
+class Login {
+    constructor(form, fields) {
+        this.form = form;
+        this.fields = fields;
+        this.validateonSubmit();
+    };
+
+    validateonSubmit() {
+        let self = this;
+
+        console.log("ciaofields")
+
+        this.form.on('submit', (event) => {
+
+            event.preventDefault();
+            var error = 0;
+
+            console.log(self.fields)
+
+            self.fields.forEach(field => {
+
+                const input = $(`#${field}`).attr('id');
+
+                if (self.validateFields(input) == false) {
+                    error++;
+                }
+            });
+
+            if (error == 0) {
+                localStorage.setItem("auth", 1);
+                this.form.submit();
+            }
+        });
+    };
+
+    validateFields(field) {
+
+        if ($(`#${field}`).val().trim() === "") {
+            this.setStatus(
+                $(`#${field}`).attr('id'),
+                
+                `${$(`#${field}`).attr('id')} cannot be blank`,
+                "error"
+            );
+
+            return false;
+
+        } else {
+            if ($(`#${field}`).attr('type') == "password") {
+                if ($(`#${field}`).val().length < 8) {
+                    this.setStatus(
+                        $(`#${field}`).attr('id'),
+                        `${$(`#${field}`).attr('id')} must be at least 8 characters`,
+                        "error"
+                    );
+
+                    return false;
+
+                } else {
+                    this.setStatus($(`#${field}`).attr('id'), null, "success");
+
+                    return true;
+                };
+            } else {
+                this.setStatus($(`#${field}`).attr('id'), null, "success");
+
+                return true;
+            };
+        };
+    };
+
+    setStatus(field, message, sta) {
+        
+        const errorMessage = $(`#${field}`).siblings(".error-message");
+    
+
+        if (sta == "success") {
+            if (errorMessage) {
+                errorMessage.text("");
+            }
+            $(`#${field}`).removeClass("input-error");
+        };
+
+        if (sta == "error") {
+            errorMessage.text(message);
+            $(`#${field}`).addClass("input-error");
+        };
+    };
+
+};
+
+//const form = document.querySelector(".loginForm");
+const form = $(".loginForm").find();
+
+console.log(form);
+
+
+
+if (form) {
+
+    const fields = ["username", "password"];
+
+    const validator = new Login(form, fields);
+}
+
+
